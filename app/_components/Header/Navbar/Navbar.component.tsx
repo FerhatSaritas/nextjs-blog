c1a9508e941dea204cc6159fe32e87cd0b09652a
Ferhat Saritas <ferhat.saritas@init.de>
2023-09-22 16:28:55 +0200

    add profile image to navbar

--- app/_components/Header/Navbar/Navbar.component.tsx
+++ app/_components/Header/Navbar/Navbar.component.tsx
@@ -1,7 +1,36 @@
+import { default as ProfilePicture } from "@app/_public/assets/profile.jpg";
+import Image from "next/image";
+
 export const Navbar = () => {
   return (
-    <div className='navbar bg-base-100'>
-      <a className='btn btn-ghost normal-case text-xl'> Insight </a>
+    <div className='navbar bg-neutral text-neutral-content w-full lg:w-11/12 mx-auto rounded-lg my-4'>
+      <div className='flex-1'>
+        <a className='btn btn-ghost normal-case text-xl'> Insight </a>
+      </div>
+      <div className='dropdown dropdown-end'>
+        <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
+          <div className='w-10 rounded-full'>
+            <Image src={ProfilePicture} width={32} alt='Profile' />
+          </div>
+        </label>
+        <ul
+          tabIndex={0}
+          className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-neutral rounded-box w-52'
+        >
+          <li>
+            <a className='justify-between'>
+              Profile
+              <span className='badge'>New</span>
+            </a>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+          <li>
+            <a>Logout</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
