d84034023c7a4293340b3855804aad3cdb638d90
Ferhat Saritas <ferhat.saritas62@gmail.com>
2023-10-03 14:19:04 +0200

    Format Date

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -19,9 +19,9 @@ export default async function Post({ params }: { params: { slug: string } }) {
     <article className='prose'>
       {bannerImage && <img src={bannerImage as string} />}
       <h1>{title}</h1>
-      <h2>
-        {author} || {date}
-      </h2>
+      <h3 className='text-gray-500'>
+        {author}, {new Date(date).toLocaleDateString("de-DE")}
+      </h3>
       <h3>{category}</h3>
       <span className='flex'>
         {tags.map((tag: string) => (
