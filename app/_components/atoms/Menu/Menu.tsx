5bca1991f91f73db8288e592e76be0733d5c2ef0
Ferhat Saritas <ferhat.saritas@init.de>
2024-01-03 16:39:08 +0100

    patch: update font of menu

--- app/_components/atoms/Menu/Menu.tsx
+++ app/_components/atoms/Menu/Menu.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Menu = () => (
   <ul
     tabIndex={0}
-    className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-neutral text-neutral-content rounded-box w-52'
+    className='menu menu-sm font-mono dropdown-content mt-3 z-[1] p-2 shadow bg-neutral text-neutral-content rounded-box w-52'
   >
     <li>
       <Link href='/blog'>Blog</Link>
